2d3017b66f084eee73ba91b32ee6a78d6ee8da16
李知祐 <jiwoo_lee@aiful.co.jp>
2024-08-27 09:33:19 +0900

    1. get photo from media and detect letter(English).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.ui)
+    implementation(libs.text.recognition)
+    implementation(libs.text.recognition.japanese)
 }
