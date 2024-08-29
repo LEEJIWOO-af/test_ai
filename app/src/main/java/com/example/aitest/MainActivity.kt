f09dd24ae7a66b55223fca5374dda9f7c263119f
李知祐 <jiwoo_lee@aiful.co.jp>
2024-08-29 13:29:47 +0900

    button and toast

--- app/src/main/java/com/example/aitest/MainActivity.kt
+++ app/src/main/java/com/example/aitest/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.aitest
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,16 +9,14 @@ import com.example.aitest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
         }
-
-
     }
 }
